4e20d99be95b407cb8e591f5968615b78caac6ab
CS9490 <cristianstatescu.cs9490@gmail.com>
2025-01-16 13:59:48 -0500

    animal launching done

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -5,17 +5,19 @@ enum ANIMAL_STATE { READY, DRAG, RELEASE }
 
 const DRAG_LIM_MAX: Vector2 = Vector2(0, 60)
 const DRAG_LIM_MIN: Vector2 = Vector2(-60, 0)
-
+const IMPULSE_MULT: float = 20.0
+const IMPULSE_MAX: float = 1200.0 # used in arrow sprite scaling -> len of impulse vector / this max to give % to scale arrow sprite 
 
 var _start: Vector2 = Vector2.ZERO
 var _drag_start: Vector2 = Vector2.ZERO
 var _dragged_vector: Vector2 = Vector2.ZERO
 var _last_dragged_vector: Vector2 = Vector2.ZERO
-
+var _arrow_scale_x : float = 0.0
 
 @onready var label = $Label
 @onready var stretch_sound = $StretchSound
 @onready var arrow = $Arrow
+@onready var launch_sound = $LaunchSound
 
 
 var _state: ANIMAL_STATE = ANIMAL_STATE.READY
@@ -23,6 +25,7 @@ var _state: ANIMAL_STATE = ANIMAL_STATE.READY
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_arrow_scale_x = arrow.scale.x
 	arrow.hide()
 	_start = position
 
@@ -33,14 +36,26 @@ func _physics_process(delta):
 	label.text += "%.1f,%.1f" % [_dragged_vector.x, _dragged_vector.y]
 
 
+func get_impulse() -> Vector2:
+	return _dragged_vector * -1 * IMPULSE_MULT
+
+func set_drag() -> void:
+	_drag_start = get_global_mouse_position()
+	arrow.show()
+
+func set_release() -> void:
+	arrow.hide()
+	freeze = false
+	launch_sound.play()
+	apply_central_impulse(get_impulse())
+	
+	
 func set_new_state(new_state: ANIMAL_STATE) -> void:
 	_state = new_state
 	if _state == ANIMAL_STATE.RELEASE:
-		arrow.hide()
-		freeze = false
+		set_release()
 	elif _state == ANIMAL_STATE.DRAG:
-		_drag_start = get_global_mouse_position()
-		arrow.show()
+		set_drag()
 
 func detect_release() -> bool:
 	if _state == ANIMAL_STATE.DRAG:
@@ -51,8 +66,13 @@ func detect_release() -> bool:
 
 
 func scale_arrow() -> void:
+	var imp_len = get_impulse().length() # Vector2
+	var perc = imp_len / IMPULSE_MAX
+	
+	arrow.scale.x = (_arrow_scale_x * perc) + _arrow_scale_x
+	
 	arrow.rotation = (_start - position).angle()
-
+	
 
 
 func play_stretch_sound() -> void:
