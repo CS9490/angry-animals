a95f2776dc82bbcd854eb57568a2e3f291b0dde2
CS9490 <cristianstatescu.cs9490@gmail.com>
2025-01-18 01:08:01 -0500

    storing high scores

--- singletons/score_manager.gd
+++ singletons/score_manager.gd
@@ -11,6 +11,7 @@ var _level_scores: Dictionary = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	load_from_disc()
 	pass # Replace with function body.
 
 func set_level_selected(ls: int) -> void:
@@ -27,7 +28,24 @@ func set_score_for_level(score: int, level: String) -> void:
 	check_and_add(level)
 	if _level_scores[level] > score: 
 		_level_scores[level] = score
+		save_to_disc()
 
 func get_best_for_level(level: String) -> int:
 	check_and_add(level)
 	return _level_scores[level]
+
+
+func save_to_disc() -> void:
+	var file = FileAccess.open(SCORES_PATH, FileAccess.WRITE)
+	var score_json_str = JSON.stringify(_level_scores)
+	file.store_string(score_json_str)
+
+func load_from_disc() -> void:
+	var file = FileAccess.open(SCORES_PATH, FileAccess.READ)
+	if file == null:
+		save_to_disc()
+	else:
+		var data = file.get_as_text()
+		_level_scores = JSON.parse_string(data)
+	
+	
